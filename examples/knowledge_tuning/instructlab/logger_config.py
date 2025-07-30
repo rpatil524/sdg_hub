209fc0da84342d145922479b50d08cc906bea6c3
Mikio Takeuchi <MTAKE@jp.ibm.com>
2025-07-31 02:41:49 +0900

    Fix pre-processing tutorial (#268)
    * Fix pre-processing tutorial
    
    * Remove unused config file
    
    * Import related changes suggested by Abhi
    
    * Convert docling v1 related code comments to markdown

---
+++ examples/knowledge_tuning/instructlab/logger_config.py
@@ -0,0 +1,15 @@
+import os
+import logging
+from rich.logging import RichHandler
+
+def setup_logger(name):
+    # Set up the logger
+    log_level = os.getenv("LOG_LEVEL", "INFO")
+    logging.basicConfig(
+        level=log_level,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler()],
+    )
+    logger = logging.getLogger(name)
+    return logger
